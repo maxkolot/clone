f925a1ce280a1cf2a49abaf2b57437acf2f8265c
maxkolot <83398783+maxkolot@users.noreply.github.com>
2023-03-17 20:46:56 +0500

    Update full.py

--- full.py
+++ full.py
@@ -1,93 +1,189 @@
-from telethon import TelegramClient, events
-import asyncio
+from xmlrpc.client import DateTime
+from telethon.sync import TelegramClient
+
+from telethon.tl.functions.messages import GetDialogsRequest
+from telethon.tl.types import InputPeerEmpty
+from telethon.tl.functions.messages import GetHistoryRequest
+from telethon.tl.types import PeerChannel
+import csv
+import os
 import time
-from random import randint
-import re 
-
-
-api_id = 4286228
-api_hash = 'fd2063177e1adb516785fc59a890842f'
-
-
-mywb= -1001833446046
-mynail = -1001745438393
-
-wb = [ 
-    -1001624439608,
-    -1001767158390,
-    -1001453526584,
-    -1001622636246,
-    -1001765559949
-    ]
-nails= [
-     -1001547526695, -1001403036444, -1001499470038, -1001143530935, -1001142275363, -1001073035721
-]
-
-client = TelegramClient('myGrab', api_id, api_hash,)
-print("GRAB - Started")
 
 
 
+api_id = 25391996
+api_hash = '72c6c97b453c642f45dd885c71e1042e'
+
+my_channel_id = -1001750094401
+channels = ["me"]
+
+# client = TelegramClient('myGrab', api_id, api_hash)
+# print("GRAB - Started")
 
 
-@client.on(events.Album(chats=wb))
-async def handler(event):
-        text1 = re.sub(r't.me\S+', 't.me/tvoya_tochka\n',event.text)
-        aaa = re.sub(r'@\S+', '[YOUR WB💕](https://t.me/tvoya_tochka)\n', text1)
-        aaa = re.sub(r'[*]', '', aaa)
+# @client.on(events.NewMessage(chats=channels))
+# async def my_event_handler(event):
 
-        await client.send_message(
-            mywb,
-            file=event.messages,
-            message =f"[YOUR WB💕](https://t.me/tvoya_tochka)\n\n{aaa}\n\nКрутые ноготочки\n ⬇ ⬇ ⬇\n[YOUR NAILS](https://t.me/your_nails_u)"
-        )
-        time.sleep(0.5)
+#     if event.message.video:
+#         await client.send_file(my_channel_id, file=event.message, caption="Самое жесткое гей порно тут👇🏻 \n[💎GPORN💎](https://t.me/gayplaygay)")
 
-     
 
-@client.on(events.NewMessage(chats=wb))
-async def my_event_handler(event):
-   
-    text1 = re.sub(r't.me\S+', 't.me/tvoya_tochka\n',event.text)
-    aaa = re.sub(r'@\S+', '[YOUR WB💕](https://t.me/tvoya_tochka)\n', text1)
-    aaa = re.sub(r'[*]', '', aaa)
+client = TelegramClient('test_session', api_id, api_hash)
 
-    if event.message.video and event.message.grouped_id == None:
-        await client.send_file(mywb, file=event.message, caption=f"[YOUR WB💕](https://t.me/tvoya_tochka)\n\n{aaa}\n\nКрутые ноготочки\n ⬇ ⬇ ⬇\n[YOUR NAILS](https://t.me/your_nails_u)")
-        
-    if event.message.photo and event.message.grouped_id == None:
-        await client.send_file(mywb, file=event.message, caption=f"[YOUR WB💕](https://t.me/tvoya_tochka)\n\n{aaa}\n\nКрутые ноготочки\n ⬇ ⬇ ⬇\n[YOUR NAILS](https://t.me/your_nails_u)")
-
-@client.on(events.Album(chats=nails))
-async def handler(event):
-        text1 = re.sub(r't.me\S+', 't.me/tvoya_tochka\n',event.text)
-        aaa = re.sub(r'@\S+', '[YOUR NAILS💕](https://t.me/your_nails_u)\n', text1)
-        aaa = re.sub(r'[*]', '', aaa)
-
-        await client.send_message(
-            mynail,
-            file=event.messages,
-            message =f"[YOUR NAILS💕](https://t.me/your_nails_u)\n\n{aaa}\n\nПоделись с подругой☺️\n\nКрутая подборка товаров WB\n ⬇ ⬇ ⬇\n[YOUR WB](https://t.me/tvoya_tochka)"
-        )
-        time.sleep(0.5)
-
-     
-
-@client.on(events.NewMessage(chats=nails))
-async def my_event_handler(event):
-   
-    text1 = re.sub(r't.me\S+', 't.me/tvoya_tochka\n',event.text)
-    aaa = re.sub(r'@\S+', '[YOUR NAILS💕](https://t.me/your_nails_u)\n', text1)
-    aaa = re.sub(r'[*]', '', aaa)
-
-    if event.message.video and event.message.grouped_id == None:
-        await client.send_file(mynail, file=event.message, caption=f"[YOUR NAILS💕](https://t.me/your_nails_u)\n\n{aaa}\n\nПоделись с подругой☺️\n\nКрутая подборка товаров WB\n ⬇ ⬇ ⬇\n[YOUR WB](https://t.me/tvoya_tochka)")
-        
-    if event.message.photo and event.message.grouped_id == None:
-        await client.send_file(mynail, file=event.message, caption=f"[YOUR NAILS💕](https://t.me/your_nails_u)\n\n{aaa}\n\nПоделись с подругой☺️\n\nКрутая подборка товаров WB\n ⬇ ⬇ ⬇\n[YOUR WB](https://t.me/tvoya_tochka)")
-    
 
 client.start()
-client.run_until_disconnected()  
 
-        
+chats = []
+last_date = None
+chunk_size = 200
+groups = []
+
+result = client(GetDialogsRequest(
+    offset_date=last_date,
+    offset_id=0,
+    offset_peer=InputPeerEmpty(),
+    limit=chunk_size,
+    hash=0
+    ))
+chats.extend(result.chats)
+
+for chat in chats:
+    try:
+        # if chat.megagroup == False:
+            groups.append(chat)
+    except:
+        continue
+
+print('Выберете группу для парсинга сообщений и членов группы:')
+i = 0
+for g in groups:
+    print(str(i) + '- ' + g.title)
+    i += 1
+
+g_index = input("Введите нужную цифру: ")
+target_group = groups[int(g_index)]
+
+i = 0
+for g in groups:
+    print(str(i) + '- ' + g.title)
+    i += 1
+
+g_index = input("Введите нужную цифру куда будем публковать ")
+# link = input("Напиши название ссылки ")
+# link1 = input("Отправь ссылку ")
+# txt = (f"{input('Напиши текст ')}\n({link})[{link1}]")
+
+txt = "[СПЯЩИЕ ПАРНИ](https://t.me/+R4o9babv-spmMTNi)"
+
+txt1 ="😈Приват спящих парней! \n▶️Стоимость -500 руб. НАВСЕГДА!\n🥵💦💦💦Более 1000 видео,\nкак спящих парней раздевают, трогают и не только)\n[ПРИВАТ](https://t.me/PRIVATsleepbot)"
+for_group = groups[int(g_index)]
+if target_group.noforwards == True:
+    gg =   client.get_messages(target_group.id)
+    i = 26
+    g = 2
+    l = 0
+    while g <=gg.total:
+            txt2 = txt
+            if l == 5:
+                 txt2 = txt1
+                 l=0
+                 
+            j =   client.get_messages(target_group.id, ids=i)
+            try:
+                if j.media != None:
+                    
+                    k = j.download_media(file="./data")
+                    
+                    client.send_file(for_group.id, f"{k}", caption=txt2)
+                    l = l+1
+                    os.remove(f"{k}")
+                    i = i + 1
+                    g = g+ 1
+                else:
+                    i = i + 1
+                    g = g+ 1
+                    continue
+            except:
+                i = i + 1
+                g = g+ 1
+                continue
+else:
+    i = 27
+    g = 2
+    k =  client.get_messages(target_group.id)
+    print (k.total)
+    l = 0
+    while g <=k.total:
+        txt2 = txt
+        if l == 5:
+                txt2 = txt1
+                l= 0
+        j =  client.get_messages(target_group.id, ids=i)
+        if j == None:
+            i = i + 1
+            g = g+ 2
+            continue
+        o = j
+        a = {}
+        hu = 0
+        if o.grouped_id != None:
+            while hu < 4:
+                
+                j =  client.get_messages(target_group.id, ids=i)
+                a[hu]=j.file
+                hu=hu + 1
+                
+            try:
+                client.send_message(for_group.id, file=a, message = txt2)
+                i = i + 1
+                g = g+ 2
+                l = l+1
+                time.sleep(2)
+            except:
+                i = i + 1
+                g = g+ 2
+                continue
+            
+            try:
+                print(f"текст: {o.text} ;;; id : {o.id} ") 
+            except:
+                pass
+        if j == None:
+            continue
+        o = j
+        if o.file != None:
+            if o.video and o.grouped_id == None or o.photo and  o.grouped_id == None:
+                
+                    try:
+                        client.send_file(for_group.id, file=o, caption=txt2)
+                        i = i + 1
+                        g = g+ 2
+                        l = l+1
+                        time.sleep(2)
+                    except:
+                        i = i + 1
+                        g = g+ 2
+                        continue
+                    try:
+                        print(f"текст: {o.text} ;;; id : {o.id} ") 
+                    except:
+                        pass
+                
+            
+            
+                # async def handler(event):
+
+                #     # craft a new message and send
+                #     await client.send_message(
+                #         for_group.id,
+                #         file=o.event.messages, # event.messages is a List - meaning we're sending an album
+                #         message=event.original_update.message.message,  # get the caption message from the album
+                    # )
+        else:
+                    
+                    i = i + 1
+                    g = g+ 2
+                    try:
+                        print(f"текст: {o.text} ;;; id : {o.id} ") 
+                    except:
+                        pass  
